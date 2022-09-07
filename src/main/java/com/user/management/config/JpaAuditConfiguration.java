5289d91b8cd5edb3c9bec966b6076494821aa4e0
Mohamed Essam <ana3ssam2017@gmail.com>
2022-09-07 15:36:45 +0200

    add AuditorAware to make audit work

--- src/main/java/com/user/management/config/JpaAuditConfiguration.java
+++ src/main/java/com/user/management/config/JpaAuditConfiguration.java
@@ -1,10 +1,23 @@
 package com.user.management.config;
 
-import lombok.RequiredArgsConstructor;
+import com.user.management.models.User;
+import com.user.management.repositories.UserRepository;
+import com.user.management.security.CurrentUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
 
 @Configuration
-@RequiredArgsConstructor
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditConfiguration {
-
+    @Autowired
+    private UserRepository userRepository;
+    @Bean
+    AuditorAware<User> auditorProvider() {
+        return () -> Optional.of(userRepository.getOne(CurrentUser.getId()));
+    }
 }
